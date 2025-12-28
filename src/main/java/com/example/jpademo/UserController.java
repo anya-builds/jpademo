f8bcbf1652af96f35dd0bf925373d7813e6d319f
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-28 23:37:43 +0530

    JPA in Spring

---
+++ src/main/java/com/example/jpademo/UserController.java
@@ -0,0 +1,4 @@
+package com.example.jpademo;
+
+public class UserController {
+}
---
+++ src/main/java/com/example/jpademo/UserService.java
@@ -0,0 +1,17 @@
+package com.example.jpademo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public UserClassSpringBoot createUser(UserClassSpringBoot user){
+      return   userRepository.save(user);
+    }
+
+}
